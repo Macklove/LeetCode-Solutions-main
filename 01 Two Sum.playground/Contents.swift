fd02d62feecad000aa70d925012a6133f2b924fc
Macklove <54309076+Macklove@users.noreply.github.com>
2022-04-10 20:49:15 +0300

    Initial commit

---
+++ 01 Two Sum.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+//Two Sum
+//https://leetcode.com/problems/two-sum
+
+class Solution {
+
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict = [Int: Int]()
+
+        for (i, num) in nums.enumerated() {
+            if let j = dict[target - num] {
+                return [j, i]
+            }
+
+            dict[num] = i
+        }
+
+        return []
+    }
+
+}
